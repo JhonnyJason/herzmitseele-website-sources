01e7d9b3a51debe3cb11d09adec6118a2557046c
Lenard Frommelt <lenny09918050@gmail.com>
2024-11-25 03:26:12 +0300

    1

--- ressources/copyscript.sh
+++ ressources/copyscript.sh
@@ -5,6 +5,7 @@
 ## The output module is the document-root of what is served on deployment
 
 mkdir -p output/img
+mkdir -p output/img/impressionen
 mkdir -p output/fonts
 mkdir -p output/audio
 mkdir -p output/video
@@ -14,6 +15,7 @@ cp sources/ressources/fonts/* output/fonts/
 cp sources/ressources/img/* output/img/
 cp sources/ressources/audio/* output/audio/
 cp sources/ressources/video/* output/video/
+cp sources/ressources/img/impressionen/* output/img/impressionen/
 
 cp sources/ressources/Datenschutzerklärung-HerzmitSeele.pdf output/Datenschutzerklärung-HerzmitSeele.pdf
 cp sources/ressources/Widerrufsbelehrung-HerzmitSeele.pdf output/Widerrufsbelehrung-HerzmitSeele.pdf
